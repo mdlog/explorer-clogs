7c2e2b649b4a57d7551ec7d9079d9f5ee452cebc
AoiNode <123106310+AoiNode@users.noreply.github.com>
2023-01-21 02:28:08 +0700

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -2,7 +2,7 @@
   <div>
     <b-card
       v-if="pingVals && pingVals.length > 0"
-      title="❤️ Helping Ping.pub By Staking ❤️"
+      title="Stake with Aoi Node"
       class="overflow-auto"
     >
       <b-table

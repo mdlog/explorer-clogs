7e35b44226398477645509ebb1249b0310a48108
AoiNode <123106310+AoiNode@users.noreply.github.com>
2023-01-21 19:11:09 +0700

    Update Staking.vue

--- src/views/Staking.vue
+++ src/views/Staking.vue
@@ -341,7 +341,7 @@ export default {
   },
   computed: {
     pingVals() {
-      return this.list.filter(x => x.description.identity === '6783E9F948541962')
+      return this.list.filter(x => x.description.identity === '40B9F7AAFF49A6E8')
     },
     list() {
       const tab = this.selectedStatus === 'active' ? this.validators : this.inactiveValidators
